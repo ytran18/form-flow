2264ed49d234828d7ea4e96158fc27bd1dce6c36
jin <a11718trannhuy@gmail.com>
2024-03-20 20:24:02 +0700

    update mobile style

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -49,8 +49,8 @@ const Login =  () => {
 
     return (
         <div className="w-screen h-screen flex items-center justify-center bg-[rgb(240,244,250)]">
-            <div className="bg-white gap-5 w-2/3 h-1/2 rounded-2xl p-8 flex flex-col ml:flex-row items-center">
-                <div className="flex flex-col gap-3 w-1/2 items-center ml:items-start">
+            <div className="bg-white gap-5 w-11/12 ml:w-2/3 h-1/2 rounded-2xl p-8 flex flex-col ml:flex-row items-center">
+                <div className="flex flex-col gap-3 ml:w-1/2 items-center ml:items-start">
                     <div className="text-3xl font-semibold">Sign in</div>
                     <div className="">to continue to Forms</div>
                 </div>
